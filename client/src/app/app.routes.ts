9a2d253d95ad53dc4bb5287c001b753f9c4da5f7
Systemized <VincentNguyen.XLV@gmail.com>
2025-06-25 12:43:01 -0400

    updated mobile UIs, completed and updated routing to about page. updated favicon, updated uis for different screen sizes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AboutComponent } from './components/about/about.component';
 import { authGuard, dashRedirect } from './services/auth.guard';
 
 export const routes: Routes = [
@@ -9,7 +10,6 @@ export const routes: Routes = [
     {path: '', component: LandingComponent, canActivate: [dashRedirect]},
     {path: 'login', component: LoginComponent},
     {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
-    {path: 'about', component: LoginComponent},
-    {path: 'features', component: LoginComponent},
+    {path: 'about', component: AboutComponent},
 
 ];
